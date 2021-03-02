a51bf65eecc88891ca1214d240b756ad0981d8c4
OzB7505 <obal7505@richmondhillhs.org>
2021-03-02 19:58:03 +0000

    divided up into components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import '../App.css';
 
-
-
-import React from 'react';
-
 function Content() {
   return (
-    <div>
-      This is an example component!
-    </div>
+    <li class="cards-item quarter">
+        <div>
+        <div class="card">
+                <div class="card-content">
+                    <div class="card-image"></div>
+                    <div class="card-container">
+                    <div class="card-title">Butcher twee cronut</div>
+                    <p class="card-text">Selfies vice chia hammock artisan ethical poutine tbh gentrify tofu.</p>
+                    <div class="btn-block">
+                        <button class="btn third">
+                        <span role="img" aria-label="Add to Album">➕</span>
+                        </button>
+                        <button class="btn third">
+                        <span role="img" aria-label="Email">✉️</span>
+                        </button>
+                        <button class="btn third">
+                        <span role="img" aria-label="Favorite">⭐</span>
+                        </button>
+                    </div>
+                    </div>
+                    <div class="btn-block">
+                    <button class="btn full">
+                        <span role="img" aria-label="Comment">💬</span> 
+                        <span class="small">[100]</span>
+                    </button>
+                    </div>
+                </div>
+                </div>
+        </div>
+    </li>
   );
 }
 
